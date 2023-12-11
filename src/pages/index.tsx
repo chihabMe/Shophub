bcea3ee22b105605758b637984e8c59b057a2feb
chihab <chihab.mg.me@gmail.com>
2023-12-11 23:59:42 +0100

    added a book add list example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,65 @@
 import { trpc } from "../utils/trpc";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-
-const HomePage = ()=>{
-  const {isLoading,data} =  trpc.books.getAllBooks.useQuery();
-  if(isLoading)return <h1>loading</h1>
-  if(!data)return <h1>error</h1>
+const HomePage = () => {
+  const { isLoading, data: books } = trpc.books.getAllBooks.useQuery();
+  if (isLoading) return <h1>loading</h1>;
+  if (!books) return <h1>error</h1>;
 
   return (
-  <div>
-      <h1>
-      {data}
-      </h1>
-  </div>
-  )
-
-  
-}
+    <div className="w-full max-w-[500px] mx-auto">
+      <AddBookBook />
+      <ul className="flex flex-col gap-2">
+        {books.map((book) => (
+          <li className="py-2 bg-gray-100 text-black text-sm ">{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+const initialForm = {
+  title: "",
+  description: "",
+};
+const AddBookBook = () => {
+  const [form, setForm] = useState(initialForm);
+  const mutateBooks = trpc.books.addBook.useMutation();
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const utils = trpc.useUtils();
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    mutateBooks.mutate(
+      {
+        title: form.title,
+        description: form.description,
+      },
+      {
+        onSuccess: () => {
+          utils.books.invalidate();
+          setForm(initialForm);
+        },
+      },
+    );
+  };
+  return (
+    <div>
+      <form className="flex flex-col gap-2" onSubmit={handleFormSubmit}>
+        <input value={form["title"]} name="title" onChange={handleInputChange} />
+        <input value={form["description"]} name="description" onChange={handleInputChange} />
+        <button
+          type="submit"
+          className="bg-blue-400 text-white font-bold px-4 py-2 rounded-md"
+        >
+          add
+        </button>
+      </form>
+    </div>
+  );
+};
 export default HomePage;
--- src/server/routes/books.ts
+++ src/server/routes/books.ts
@@ -1,6 +1,27 @@
 import { prisma } from "@/src/utils/prisma";
 import { publicProcedure, router } from "../trpc";
+import * as z from "zod";
+import { TRPCError } from "@trpc/server";
+
 export const booksRouter = router({
-  getAllBooks: publicProcedure.query(async() => {
+  getAllBooks: publicProcedure.query(async () => {
+    return prisma.book.findMany();
   }),
+  addBook: publicProcedure
+    .input(z.object({ title: z.string(), description: z.string() }))
+    .mutation(async (ctx) => {
+      const author = await prisma.author.findFirst();
+      if (!author)
+        throw new TRPCError({
+          message: "no author",
+          code: "BAD_REQUEST",
+        });
+      return prisma.book.create({
+        data: {
+          title: ctx.input.title,
+          description: ctx.input.description,
+          authorId: author.id,
+        },
+      });
+    }),
 });
